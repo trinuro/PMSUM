be6b7302b9af4a47d36f2aa4d69b66ed38ed0e66
trinuro <66290935+trinuro@users.noreply.github.com>
2024-08-22 15:32:22 +0800

    Added CSS configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,17 @@ const config: Config = {
         leagueSpartan: ['var(--font-league)'],
         horta: ['var(--font-horta)'],
       },
+      screens: {
+        xs: '400px',
+        '3xl': '1680px',
+        '4xl': '2200px',
+      },
+      maxWidth: {
+        '10xl': '1512px',
+      },
+      borderRadius: {
+        '5xl': '40px',
+      },
     },
   },
   plugins: [],
