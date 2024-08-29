6f10cb858df44a412388a75ad8e53a421f11d26e
ngyuheng2003 <ngyuheng2003@gmail.com>
2024-08-29 15:22:52 +0800

    Activities: Committee component updated

--- components/activities/activities_committee.tsx
+++ components/activities/activities_committee.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import Slider from "react-slick"; //npm install react-slick --save && npm install @types/react-slick --save
 import "slick-carousel/slick/slick.css"; // npm install slick-carousel
 import "slick-carousel/slick/slick-theme.css";
+// npm install webpack-node-externals --save-dev
 
 const activities_committee = (props: any) => {
   var settings = {
@@ -36,7 +37,7 @@ const activities_committee = (props: any) => {
   var url = "/image/activities/act1/committee/"+ selected
 
   return (
-    <div className='max-container h-[600px] flex content-center p-10'>
+    <div className='max-container h-[650px] flex content-center p-10'>
       <div className='w-full'>
         <div>
           <div className='font-bold text-3xl'>Meet the committee</div>
@@ -50,11 +51,11 @@ const activities_committee = (props: any) => {
           </select>
         </div>
         <div className='mt-8 flexCenter bg-yellow-dark bg-opacity-50'>
-          <div className='w-[320px] h-[400px] xl:w-[1300px] content-center'>
+          <div className='w-[320px] h-[400px] xl:w-[1250px] content-center'>
             <Slider {...settings}>
               {props.image.map((each: string, index: number) => (
                 <div key={index}>
-                  <div className='w-full flex justify-center' >
+                  <div className='w-full flex justify-center p-10' >
                     <img className='size-[320px] transition hover:scale-110' src={each}></img>
                   </div>
                 </div>
