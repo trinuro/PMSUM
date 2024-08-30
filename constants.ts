23dc6d503bbad2fac2cab1802a2cbdade0c07608
trinuro <66290935+trinuro@users.noreply.github.com>
2024-08-30 15:46:48 +0800

    Fixed typo

--- constants.ts
+++ constants.ts
@@ -124,6 +124,8 @@ export const BioAutographyList: BioAutographies = [
     linkedin: "#",
     autobiography: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,"
   }
+]
+
 export const OBJECTIVES = [
     'Lorem ipsum dolor sit amet, consectetur ing elit. Aliquam sit amet dignissim turpis.',
     'Lorem ipsum dolor sit amet, consectetur ing elit. Aliquam sit amet dignissim turpis.',
