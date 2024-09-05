be3bbc2e93bcfa6efc1b90f73a48b0093cc4a25e
HamZhiWei33 <149857735+HamZhiWei33@users.noreply.github.com>
2024-09-05 21:52:07 +0800

    delete unused code which is commented previously

--- components/navbar/IconDiskusiBelanjawan.tsx
+++ components/navbar/IconDiskusiBelanjawan.tsx
@@ -1,30 +1,3 @@
-// "use client"
-
-// import { useState } from 'react';
-// import Image from 'next/image';
-
-// const IconDiskusiBelanjawan = () => {
-//   const [isHovered, setIsHovered] = useState(false);
-
-//   return (
-//     <div
-//       className="flex items-center justify-center cursor-pointer"
-//       onMouseEnter={() => setIsHovered(true)}
-//       onMouseLeave={() => setIsHovered(false)}
-//     >
-//       <Image
-//         src={isHovered ? '/icon/icon_diskusi_belanjawan_red.svg' : '/icon/icon_diskusi_belanjawan_grey.svg'}
-//         alt="Logo"
-//         width={100} 
-//         height={100}
-//         className="transition-opacity duration-300 ease-in-out"
-//       />
-//     </div>
-//   );
-// };
-
-// export default IconDiskusiBelanjawan;
-
 "use client";
 
 import Image from 'next/image';
--- components/navbar/IconFederalisme.tsx
+++ components/navbar/IconFederalisme.tsx
@@ -1,30 +1,3 @@
-// "use client"
-
-// import { useState } from 'react';
-// import Image from 'next/image';
-
-// const IconFederalisme = () => {
-//   const [isHovered, setIsHovered] = useState(false);
-
-//   return (
-//     <div
-//       className="flex items-center justify-center cursor-pointer"
-//       onMouseEnter={() => setIsHovered(true)}
-//       onMouseLeave={() => setIsHovered(false)}
-//     >
-//       <Image
-//         src={isHovered ? '/icon/icon_federalisme_di_Malaysia_red.svg' : '/icon/icon_federalisme_di_Malaysia_grey.svg'}
-//         alt="Logo"
-//         width={100} 
-//         height={100}
-//         className="transition-opacity duration-300 ease-in-out"
-//       />
-//     </div>
-//   );
-// };
-
-// export default IconFederalisme;
-
 "use client";
 
 import Image from 'next/image';
