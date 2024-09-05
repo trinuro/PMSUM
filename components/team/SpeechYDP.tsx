9a356714ed6a4f0ea8915a9edc78bfa968d5304c
HamZhiWei33 <149857735+HamZhiWei33@users.noreply.github.com>
2024-09-05 21:59:07 +0800

    paragraph is centered vertically

--- components/team/SpeechYDP.tsx
+++ components/team/SpeechYDP.tsx
@@ -1,56 +1,34 @@
-// import Image from "next/image";
-
-// const SpeechYDP = () => {
-//     return (
-//         <div className="w-full xl:h-screen">
-            
-//             <div className="flex flex-wrap md:flex-none">
-//                 <div className="flex-1 flexCenter px-1 md:block">
-//                     <h1 className="uppercase bold-28 md:bold-52 text-left md:text-center md:py-8 ml-4 md:m-2">Who We Are</h1>
-//                 </div>
-//                 <div className="flex-1 flexCenter md:flex-1">
-//                     <Image src="/image/team/speech_YDP.png" alt="d" width={200} height={220}/>
-//                 </div>
-                
-//             </div>
-//             <div className="flexCenter md:flex-1 m-2 mt-8 p-4">
-//                     <p className="regular-16 md:regular-18">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur et rem esse hic asperiores, facere nesciunt. Ab doloremque eum molestiae quod optio sit, tenetur necessitatibus ullam soluta non nisi accusamus qui exercitationem voluptatum. Aliquam fugit nihil libero numquam enim nulla debitis beatae ad, eveniet hic earum non deserunt facere animi.</p>
-//                 </div>
-            
-//         </div>
-//     );
-// }
-
-// export default SpeechYDP;
-
+"use client";
 import Image from "next/image";
 
 const SpeechYDP = () => {
     return (
-        <div className="w-full xl:h-screen p-4">
+        <div className="w-full h-3/4 p-4">
             {/* Wrapper for medium screens and up */}
             <div className="md:flex md:flex-col md:items-center">
                 {/* Heading Section */}
-                <div className="text-center md:mb-8">
-                    <h1 className="uppercase bold-28 md:bold-52">Who We Are</h1>
+                <div className="text-center my-4 md:mb-8">
+                    <h1 className="my-element uppercase font-[700] leading-[120%] text-4xl md:text-5xl">
+                        Who We Are
+                    </h1>
                 </div>
 
                 {/* Image and Paragraph Section */}
-                <div className="md:flex md:items-start md:justify-center w-full">
+                <div className="md:flex md:items-center md:justify-center w-full">
                     {/* Image Section */}
-                    <div className="md:w-1/3 flex justify-center md:justify-end mb-4 md:mb-0">
+                    <div className="md:w-1/3 flex justify-center md:justify-end mb-8 md:mb-0">
                         <Image 
                             src="/image/team/speech_YDP.png" 
                             alt="Who We Are" 
-                            width={200} 
-                            height={200} 
-                            className="object-contain md:w-[300px] xl:w-[400px]"
-                            />
+                            width={250} 
+                            height={250} 
+                            className="object-contain md:w-[300px] lg:w-[400px] 2xl:w-[600px]"
+                        />
                     </div>
 
                     {/* Paragraph Section */}
-                    <div className="md:w-2/3 md:ml-4">
-                        <p className="text-base md:regular-18">
+                    <div className="md:w-2/3 mx-8 my-2 md:ml-4">
+                        <p className="text-base lg:text-lg 2xl:text-2xl">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur et rem esse hic asperiores, facere nesciunt. Ab doloremque eum molestiae quod optio sit, tenetur necessitatibus ullam soluta non nisi accusamus qui exercitationem voluptatum. Aliquam fugit nihil libero numquam enim nulla debitis beatae ad, eveniet hic earum non deserunt facere animi.
                         </p>
                     </div>
