ce913d38389f79b957033184ec10dbdbfa2aaa8d
ngyuheng2003 <ngyuheng2003@gmail.com>
2024-09-06 18:40:39 +0800

    Activities: build error fixed

--- app/activities/details/page.tsx
+++ app/activities/details/page.tsx
@@ -23,30 +23,31 @@ export default function Activities() {
 }
 
 const Act = () => {
-  var act_code = useSearchParams().get('code')
-  if(act_code == null){
-    act_code = "1"
-  }
   const act = require('@/components/info/activities/info_activity')
-  return (
-    <div>
+  var act_code = useSearchParams().get('code')
+  if (act_code == null) {
+    return <></>
+  }
+  else
+    return (
       <div>
-        <Cover code={act_code} title={act[act_code].title} date={act[act_code].date} location={act[act_code].location} video_url={act[act_code].video_url} />
+        <div>
+          <Cover code={act_code} title={act[act_code].title} date={act[act_code].date} location={act[act_code].location} video_url={act[act_code].video_url} />
+        </div>
+        <div id='banner'>
+          <Message message={act[act_code].message} />
+          <Banner code={act_code} src={act[act_code].banner_image_source} title={act[act_code].title} subtitle={act[act_code].subtitle} />
+          <Title code={act_code} title={act[act_code].title} date={act[act_code].date} description={act[act_code].description} icon={act[act_code].icon} />
+          <Objective content={act[act_code].objective} />
+          <Infographic info={act[act_code].infographic} />
+          <Youtube id={act[act_code].youtube_id} ack={act[act_code].youtube_acknowledgement} />
+          <Highlight content={act[act_code].highlight} />
+          <Gallery gallery={act[act_code].gallery} caption={act[act_code].gallery_caption} link={act[act_code].gallery_link} download_name={act[act_code].gallery_download_name} />
+          <Committee code={act_code} list={act[act_code].committee_list} image={act[act_code].committee_image} />
+          <SocialMedia code={act_code} insta={act[act_code].instagram_id} bg_image={act[act_code].instagram_bg_image} />
+        </div>
       </div>
-      <div id='banner'>
-        <Message message={act[act_code].message} />
-        <Banner code={act_code} src={act[act_code].banner_image_source} title={act[act_code].title} subtitle={act[act_code].subtitle} />
-        <Title code={act_code} title={act[act_code].title} date={act[act_code].date} description={act[act_code].description} icon={act[act_code].icon} />
-        <Objective content={act[act_code].objective} />
-        <Infographic info={act[act_code].infographic} />
-        <Youtube id={act[act_code].youtube_id} ack={act[act_code].youtube_acknowledgement} />
-        <Highlight content={act[act_code].highlight} />
-        <Gallery gallery={act[act_code].gallery} caption={act[act_code].gallery_caption} link={act[act_code].gallery_link} download_name={act[act_code].gallery_download_name} />
-        <Committee code={act_code} list={act[act_code].committee_list} image={act[act_code].committee_image} />
-        <SocialMedia code={act_code} insta={act[act_code].instagram_id} bg_image={act[act_code].instagram_bg_image} />
-      </div>
-    </div>
 
-  )
+    )
 }
 
