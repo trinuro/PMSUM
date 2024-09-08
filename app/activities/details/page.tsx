3fb4fe2ad0c81dbfce1dbc89c1b6d2f0fe2aad5c
ngyuheng2003 <ngyuheng2003@gmail.com>
2024-09-08 22:55:58 +0800

    Activities: images updated

--- app/activities/details/page.tsx
+++ app/activities/details/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
 import Banner from '@/components/activities/details/activities_banner';
 import Committee from '@/components/activities/details/activities_committee';
 import Cover from '@/components/activities/details/activities_cover'
@@ -13,9 +14,36 @@ import SocialMedia from '@/components/activities/details/activities_socialMedia'
 import Title from '@/components/activities/details/activities_title';
 import Youtube from '@/components/activities/details/activities_youtube';
 
-import { useSearchParams } from 'next/navigation';
-
 export default function Activities() {
+
+  const Act = () => {
+    const act = require('@/components/info/activities/info_activity')
+    const act_code = useSearchParams().get('code')
+    if (act_code == null || undefined || "" || act[act_code] == null) {
+      return <NotFound />
+    }
+    else
+      return (
+        <div className='max-w-screen overflow-hidden'>
+          <div>
+            <Cover act_code={act_code} />
+          </div>
+          <div id='banner'>
+            <Message act_code={act_code} />
+            <Banner act_code={act_code} />
+            <Title act_code={act_code} />
+            <Objective act_code={act_code} />
+            <Infographic act_code={act_code} />
+            <Youtube act_code={act_code} />
+            <Highlight act_code={act_code} />
+            <Gallery act_code={act_code} />
+            <Committee act_code={act_code} />
+            <SocialMedia act_code={act_code} />
+          </div>
+        </div>
+      )
+  }
+
   return (
     <main>
       <Suspense><Act /></Suspense>
@@ -23,32 +51,5 @@ export default function Activities() {
   );
 }
 
-const Act = () => {
-  const act = require('@/components/info/activities/info_activity')
-  const act_code = useSearchParams().get('code')
-  if (act_code == null||undefined||"" || act[act_code] == null) {
-    return <NotFound/>
-  }
-  else
-    return (
-      <div>
-        <div>
-          <Cover act_code={act_code}/>
-        </div>
-        <div id='banner'className='max-w-screen overflow-hidden'>
-          <Message act_code={act_code}/>
-          <Banner act_code={act_code}/>
-          <Title act_code={act_code}/>
-          <Objective act_code={act_code}/>
-          <Infographic act_code={act_code}/>
-          <Youtube act_code={act_code}/>
-          <Highlight act_code={act_code}/>
-          <Gallery act_code={act_code}/>
-          <Committee act_code={act_code}/>
-          <SocialMedia act_code={act_code}/>
-        </div>
-      </div>
 
-    )
-}
 
---
+++ components/info/activities/info_activity_template.ts
@@ -0,0 +1,148 @@
+/* General component
+
+export const act[index] = {
+  title: "",
+  subtitle: "",
+  icon: "",
+  date: "",
+  location: "",
+  description: [
+    "",
+  ],
+  video_url: "",
+
+  // Add other component(s) here
+
+}
+
+*/
+
+/* Committee component
+
+ committee_list: [
+    'Majlis Tertinggi',
+    'Jemputan dan Protokol',
+    'Promosi dan Publisiti',
+    'Jamuan dan Cenderamata',
+    'Penyertaan dan Pendaftaran',
+    'Teknikal',
+    'Logistik dan Pengangkutan',
+    'Tanjaan'
+  ],
+
+  committee_image: {
+    'Majlis Tertinggi': {
+      code: "mtt",
+      total: ,
+    },
+    'Jemputan dan Protokol': {
+      code: "jnp",
+      total: ,
+    },
+    'Promosi dan Publisiti': {
+      code: "pnp",
+      total: ,
+    },
+    'Jamuan dan Cenderamata': {
+      code: "jnc",
+      total: ,
+    },
+    'Penyertaan dan Pendaftaran': {
+      code: "pep",
+      total: ,
+    },
+    'Teknikal': {
+      code: "tek",
+      total: ,
+    },
+    'Tanjaan': {
+      code: "tan",
+      total: ,
+    },
+    'Logistik dan Pengangkutan': {
+      code: "log",
+      total: ,
+    },
+  },
+
+*/
+
+/* Gallery component
+
+  gallery: [
+    ["/image/activities/act1/img_act1_1.jpg", "Multimedia PMSUM"],
+    ["/image/activities/act1/img_act1_2.JPG", "Multimedia PMSUM"],
+    ["/image/activities/act1/img_act1_4.JPG", "Multimedia PMSUM"],
+    ["/image/activities/act1/img_act1_5.JPG", "Multimedia PMSUM"],
+    ["/image/activities/act1/img_act1_6.JPG", "Multimedia PMSUM"],
+    ["/image/activities/act1/img_act1_7.JPG", "Multimedia PMSUM"],
+    ["/image/activities/act1/img_act1_8.JPG", "Multimedia PMSUM"],
+    ["/image/activities/act1/img_act1_9.JPG", "Multimedia PMSUM"],
+    ["/image/activities/act1/img_act1_10.JPG", "Multimedia PMSUM"],
+  ],
+  gallery_download_name: 'dbs2024',
+  gallery_caption: `
+    Hampir 3 minggu sudah berlalu semenjak Diskusi Belanjawan Sarawak melabuhkan tirainya.
+    Sudah tentu ramai dari kita masih merindui detik-detik pada hari berlangsungnya program?
+    Mari kita belek semula gambar-gambar yang sempat dirakam lensa kamera sepanjang program Diskusi Belanjawan Sarawak!
+    Mana tahu, akan jumpa muka anda juga!
+  `,
+  gallery_link: "https://drive.google.com/drive/folders/1j1jhewuUsHYePCprJKnzn9xYjZK0A2Or?usp=sharing",
+  
+
+*/
+
+/* Highlight component
+
+highlight: [
+    [{
+      image_url: "/image/activities/act1/img_act1_1.jpg",
+      title: "Sarawak Maju Makmur: Rahsia Kerajaan Surplus",
+      subtitle: "Speaker: Yang Amat Berhormat Datuk Patinggi Tan Sri (DR) Abang Haji Abdul Rahman Zohari Bin Tun Datuk Abang Haji Openg",
+      content: "Yang Amat Berhormat Datuk Patinggi Tan Sri (DR) Abang Haji Abdul Rahman Zohari Bin Tun Datuk Abang Haji Openg shared the secrets of becoming a state with economic surplus in his keynote speech. In this session, undergraduate students had the opportunity to learn more about the Sarawakian economy and other matters that concerned them.",
+    }],
+    [{
+      image_url: "/image/activities/act1/img_act1_2.JPG",
+      title: "DBS: Ke Arah Kemakmuran Wilayah",
+      subtitle: "Speaker: YB Dr. Kelvin Yii Lee Wuen Puan Kho Teck Wan YB Datuk. Dr. Hj. Hazland bin Abang Hj. Hipni",
+      content: "In this discussion session, the Sarawakian budget was discussed and analyzed comprehensively from multiple perspectives. Among the topics discussed are the green economy, hydrogen fuel and public transportation.",
+    }],
+    [{
+      image_url: "/image/activities/act1/img_act1_3.jpg",
+      title: "Ekonomi Hidrogen: Solusi atau Sensasi",
+      subtitle: "Speaker: Saudari Nurul Qurratu'aini Saudara Mohd Nur Azuwan",
+      content: "A debate session was held between two undergraduate students to have a debate on the economic policy of Sarawak. It encourages students to think critically about the hydrogen economy from multiple viewpoints.",
+    }],
+  ],
+
+*/
+
+/* Message component
+
+  message: [
+    "",
+    ],
+
+*/
+
+/* Objective component 
+
+  objective: [
+    "",
+  ],
+
+*/
+
+/* Social Media component
+
+  instagram_id: "",
+  instagram_bg_image: "banner",
+
+*/
+
+/* Youtube component
+
+  youtube_id: "",
+  youtube_acknowledgement: "",
+
+*/
