13783b921a117190ad50a8b5945b595162ba2b30
trinuro <66290935+trinuro@users.noreply.github.com>
2024-09-12 13:51:01 +0800

    Fix bug where the title overlaps with dayak strip

--- components/main_page/hero.tsx
+++ components/main_page/hero.tsx
@@ -1,11 +1,11 @@
 const Hero = () =>{
     return (<section className="bg-hero-bg h-screen relative -mt-[80px]">
         {dayak_strip('h-20 lg:h-32 top-0 opacity-40')}
-        <section className="flex max-container padding-container justify-between pt-36">
+        <section className="flex max-container padding-container justify-between pt-[20vh]">
             <div className="font-extrabold z-20 [text-shadow:_0_5px_2px_rgb(0_0_0_/_40%)]">
                 <p className="text-white italic text-[12px] md:text-[23px] lg:text-[38px]">AN ASSOCIATION OF SARAWAKIANS</p>
-                <h1 className="text-yellow-dark text-[32px] leading-[120%] md:text-[70px] lg:text-[114px]">BY SARAWAKIANS</h1>
-                <h1 className="text-red-dark text-[30px] leading-[120%] md:text-[66px] lg:text-[107px]">FOR SARAWAKIANS</h1>
+                <h1 className="text-yellow-dark text-[32px] leading-[120%] md:text-[70px] lg:text-[90px] xl:text-[114px]">BY SARAWAKIANS</h1>
+                <h1 className="text-red-dark text-[30px] leading-[120%] md:text-[66px] lg:text-[85px] xl:text-[107px]">FOR SARAWAKIANS</h1>
                 <p className="text-white italic text-[12px] md:text-[23px] lg:text-[38px] text-right">IN UNIVERSITI MALAYA</p>
             </div>
         </section>
